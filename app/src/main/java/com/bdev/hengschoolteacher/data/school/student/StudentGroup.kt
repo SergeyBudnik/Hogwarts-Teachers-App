ca5f6a51b53f5aac834cffaac9307a94c5052d7e
Sergey <serbudnik@gmail.com>
2019-07-17 13:27:32 +0300

    .

--- app/src/main/java/com/bdev/hengschoolteacher/data/school/student/StudentGroup.kt
+++ app/src/main/java/com/bdev/hengschoolteacher/data/school/student/StudentGroup.kt
@@ -6,5 +6,5 @@ import org.codehaus.jackson.annotate.JsonProperty
 class StudentGroup @JsonCreator constructor(
         @JsonProperty("groupId") val groupId: Long,
         @JsonProperty("startTime") val startTime: Long,
-        @JsonProperty("finishTIme") val finishTime: Long?
+        @JsonProperty("finishTime") val finishTime: Long?
 )
