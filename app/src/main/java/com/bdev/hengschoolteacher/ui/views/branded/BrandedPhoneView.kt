0460c6bc910987b0fb44978ea68fe9e23f8f2444
budnser <serbudnik@gmail.com>
2020-10-13 13:16:12 +0300

    phones name long tap on phone to copy to clipboard

--- app/src/main/java/com/bdev/hengschoolteacher/ui/views/branded/BrandedPhoneView.kt
+++ app/src/main/java/com/bdev/hengschoolteacher/ui/views/branded/BrandedPhoneView.kt
@@ -1,9 +1,13 @@
 package com.bdev.hengschoolteacher.ui.views.branded
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.bdev.hengschoolteacher.R
+import com.bdev.hengschoolteacher.data.school.person.PersonContact
 import com.bdev.hengschoolteacher.ui.activities.BaseActivity
 import com.bdev.hengschoolteacher.ui.utils.PhoneUtils
 import kotlinx.android.synthetic.main.view_branded_phone.view.*
@@ -14,10 +18,24 @@ open class BrandedPhoneView : LinearLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    fun bind(phone: String) : BrandedPhoneView {
-        brandedPhoneTextView.text = phone
+    fun bind(personContact: PersonContact) : BrandedPhoneView {
+        brandedPhoneNameView.text = personContact.name
+        brandedPhoneValueView.text = personContact.value
 
-        setOnClickListener { PhoneUtils.call(context as BaseActivity, phone) }
+        setOnClickListener {
+            PhoneUtils.call(context as BaseActivity, personContact.value)
+        }
+
+        setOnLongClickListener {
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(personContact.value, personContact.value)
+
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(context, "Телефон скопирован", Toast.LENGTH_SHORT).show()
+
+            return@setOnLongClickListener true
+        }
 
         return this
     }
